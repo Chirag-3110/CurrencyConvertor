c99c3710c609a8fefe7e0ab7fc859c2055c213ec
Chirag <takchirag828@gmail.com>
2022-12-29 00:05:17 +0530

    final readme

---
+++ README.md
@@ -0,0 +1,6 @@
+This is Currency convertor app develop in react native
+for runnig the app in debug mode follow the following steps:    
+    1.run npm install after tabing clone of repository.
+    2.run npx react-native run-android
+
+Drive Link for apk
--- src/Data/currency.js
+++
@@ -1,8 +0,0 @@
-const currency=[
-    {id:1,name:"pound",country:"England",priority:5},
-    {id:2,name:"euro",country:"Europe",priority:4},
-    {id:3,name:"doller",country:"American",priority:3},
-    {id:4,name:"ruppee",country:"India",priority:2},
-    {id:5,name:"Japanese Yen",country:"Japan",priority:1},
-]
-export default currency;
